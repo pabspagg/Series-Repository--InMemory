6c92c3c297b496bb898cb87160c3f0d4ecc97342
pabspagg <pabspagg@gmail.com>
2022-02-11 15:06:29 -0300

    Add project files.

---
+++ DIO.Series/Classes/Serie.cs
@@ -0,0 +1,47 @@
+﻿using Dio.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series.Classes
+{
+     public class Serie:EntidadeBase
+    {
+     
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+
+        public bool Excluido { get; set; }
+
+        //Construtor
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string? ToString()
+        {
+            string retorno = String.Empty;
+            retorno+= "Genêro: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido:" + this.Excluido;
+            return retorno;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
---
+++ DIO.Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornarPorId(int id);
+        void Insere(T obj);
+        void Exclui(int id);
+        void Atualiza(int id, T obj);
+        int ProximoId();
+    }
+}
